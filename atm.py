bddbf61969f4e11721de91baec31c68eadaa6a71
sharana272 <basavasharana272@gmail.com>
2025-07-06 22:12:48 +0530

    atm.py

---
+++ atm.py
@@ -0,0 +1,59 @@
+balance=10000
+pin =1234
+
+print("wlecome to atm")
+print("insert card here")
+if("card inserted"):
+      print("yes")
+else:
+     print("no")
+
+print(" enter pin")
+pin_input = int(input("enter pin"))
+if("pin is correct"):
+      print("pin is correct")
+else:
+      print("no")
+
+print("select language")
+print("1.english")
+print("2.kannada")
+print("3.hindi")
+if("1 "):
+    print("selected 1.english")
+elif("2"):
+     print("selected 2.kannada")
+elif("3"):
+        print("selected 3.hindi")
+else:
+   print("1.")
+
+print("select option")
+print("1.withdraw")
+print("2.deposit")
+print("3.balance")
+if("1 "):
+    print("selected 1.deposit")
+elif("2"):
+     print("slected 2.withdraw")
+elif("3"):
+     print("selected 3.balance")
+else:
+     print("1.")
+print("enter amount to withdraw")
+withdraw =int(input("withdraw"))
+
+print("withdraw successful")
+print("do you want check balance")
+if("check balance"):
+ print("yes")
+else:
+ print("no")
+ 
+print("your balance is", withdraw-balance)
+print("please collect your card")
+print("thank you for using atm")
+
+
+
+
