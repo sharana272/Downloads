eb00571565269697aa9c4b1ed2bcb5cd5f0e8550
sharana272 <basavasharana272@gmail.com>
2025-07-09 19:26:00 +0530

    add programa1.py

--- programa1.py
+++ programa1.py
@@ -3,3 +3,32 @@ print(88.5+99.2)#addition
 print(6+2+3+9+88.7+0)#addition
 print(+9)#positive number
 print(+97.6)#positive number
+
+print(99-2-55-3)#substraction
+print(23-56)#substraction
+print(100.8-33.2-32.9)#substraction
+print(-98.6)#negative number
+print(-8)#negative number
+
+print(10*6*3*3*4*18)#multipliction
+print(22.7*33.2)#multipliction
+print(8*88.6)#multipliction
+
+
+print(2**3)
+print(3**2)
+print(3.2**2)
+
+print(22//7)#return non decimal quotient
+print(10//2)
+
+print(22/7)#return  decimal quotient
+print(10/2)
+
+print(22%7)#return  remainder
+print(10%2)
+print(5%888)
+print(11%5009)
+
+
+
