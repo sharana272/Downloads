83a3024381e21b57a378a4d3bdeeb2c8ab1e40e1
sharana272 <basavasharana272@gmail.com>
2025-07-10 11:19:01 +0530

    added new concept (Creation of Class)

---
+++ swclass.py
@@ -0,0 +1,9 @@
+class student:
+    def __init__(self, name, age, mobile, address):
+        self.name = name
+        self.age = age
+        self.mobile = mobile
+        self.address = address
+
+s1 = student("rama", 30, 99744, "beng")
+print(s1.name)
